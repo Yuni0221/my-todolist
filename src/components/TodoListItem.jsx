801db21cf36ee09757d4f971e7cf99fd1d60b96c
Yuni0221 <moonrise002002@gmail.com>
2023-07-06 14:45:04 +0900

    Feat: Implement checked & delete function

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,18 +1,36 @@
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { GrFormAdd } from "react-icons/gr";
+import { AiOutlineCheck } from "react-icons/ai";
+import { AiFillDelete } from "react-icons/ai";
 
 export default function TodoListItem() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [completedItems, setCompletedItems] = useState([]);
 
   const handleChangeTodo = (event) => {
     setTodo(event.target.value);
     console.log(event.target.value);
   };
 
+  const handleCheckboxChange = (item) => {
+    if (completedItems.includes(item)) {
+      setCompletedItems(
+        completedItems.filter((completedItem) => completedItem !== item)
+      );
+    } else {
+      setCompletedItems([...completedItems, item]);
+    }
+  };
+
+  const handleClickDelete = (item) => {
+    setTodos(todos.filter((todo) => todo !== item));
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (todo === "") {
+    if (todo.trim() === "") {
       return;
     } else {
       setTodos([...todos, todo]);
@@ -20,30 +38,49 @@ export default function TodoListItem() {
     }
   };
   return (
-    <div>
-      <ul>
-        {todos.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={todo} onChange={handleChangeTodo}></input>
-        <button>등록</button>
-      </form>
-    </div>
-  );
-}
-
-{
-  /* <input
-            className={twMerge(
-              `w-[440px] h-[70px] bg-Beige rounded-[20px] mb-8 pl-6 outline-none`
-            )}
+    <>
+      <div className={`relative`}>
+        <ul className={"w-[400px] [500px] pt-10"}>
+          {todos.map((item) => (
+            <li className={"flex items-center p-1"}>
+              <input
+                className={"mr-2 w-4 h-4  accent-pink-500 rounded-lg"}
+                type="checkbox"
+                onChange={() => handleCheckboxChange(item)}
+                checked={completedItems.includes(item)}
+              ></input>
+              <div
+                className={completedItems.includes(item) ? "line-through" : ""}
+              >
+                {item}
+              </div>
+              <div className={"flex ml-auto"}>
+                <AiFillDelete
+                  className={" text-gray-600 cursor-pointer"}
+                  onClick={() => handleClickDelete(item)}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+        <form
+          onSubmit={handleSubmit}
+          className="flex justify-center items-center absolute w-[100%] top-auto left-0 bottom-6"
+        >
+          <input
+            placeholder="what is your plan?"
             type="text"
             value={todo}
-            onChange={handleChange}
+            onChange={handleChangeTodo}
+            className={twMerge(
+              `w-[400px] h-[70px] bg-Beige rounded-[28px] mr-4 p-4 outline-none`
+            )}
           ></input>
-          <button className="absolute w-[44px] h-[44px] ml-[340px] mb-11 bg-Blue rounded-full">
-            <GrFormAdd className="w-[30px] h-[30px] ml-2" />
-          </button> */
+          <button className="flex justify-center w-[50px] h-[44px] bg-Blue rounded-full items-center">
+            <GrFormAdd className="w-[30px] h-[30px]" />
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
