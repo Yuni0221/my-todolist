69439b03070dbb135983d6eadf84e4b659a4a23b
Yuni0221 <moonrise002002@gmail.com>
2023-07-11 09:51:47 +0900

    Feat: Implement image update function

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
-import { FaUserEdit } from "react-icons/fa";
+import { FaUserEdit, FaUserPlus } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
 import TodoListItem from "../components/TodoListItem";
 import Weather from "../components/Weather";
 
 export default function Main({ props }) {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageFile, setImageFile] = useState(null);
   const location = useLocation();
   const { name } = location.state.key;
 
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const imageUrl = e.target.result;
+      setImageUrl(imageUrl);
+      setImageFile(file);
+    };
+    reader.readAsDataURL(file);
+  };
   return (
     <section>
       <section className="w-screen h-screen flex justify-center items-center bg-Beige">
@@ -24,9 +37,29 @@ export default function Main({ props }) {
             >
               <div
                 className={twMerge(
-                  `flex w-[100px] h-[100px] bg-Beige rounded-full mr-6`
+                  `flex justify-center items-center w-[100px] h-[100px] bg-Beige rounded-full mr-6`
                 )}
-              ></div>
+              >
+                {imageUrl ? (
+                  <img
+                    src={imageUrl}
+                    alt="Uploaded Image"
+                    className="w-[100px] h-[100px] rounded-full"
+                  />
+                ) : (
+                  <div className="relative">
+                    <FaUserPlus
+                      className="w-[30px] h-[30px] text-Gray cursor-pointer"
+                      onChange={handleImageUpload}
+                    />
+                    <input
+                      type="file"
+                      onChange={handleImageUpload}
+                      className="flex absolute items-center w-[30px] h-[30px] inset-0 opacity-0 cursor-pointer"
+                    />
+                  </div>
+                )}
+              </div>
               <div className={`w-[160px] font-bold text-lg text-Beige`}>
                 Welcome, {name} 님!
               </div>
